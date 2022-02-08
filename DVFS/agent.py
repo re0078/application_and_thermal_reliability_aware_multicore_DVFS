ba78364abc3ee8e7f7d15e868cf17075d70b8469
reza <reza.abdollahzadeh12@gmail.com>
2022-02-08 22:59:58 +0330

    RL agent loop completed

--- DVFS/agent.py
+++ DVFS/agent.py
@@ -1,10 +1,10 @@
 import itertools
 from collections import defaultdict
+from time import time_ns
+from typing import List
 
 import numpy as np
 import plotting
-from time import time_ns
-from typing import List
 
 from DVFS.models.models import ThermalReliability, ApplicationReliability
 from DVFS.power_prediction import PowerPredictor
@@ -12,8 +12,8 @@ from DVFS.power_prediction import PowerPredictor
 _POWER_KEY, _TR_KEY, _AR_KEY = 0, 1, 2
 
 
-def createEpsilonGreedyPolicy(Q, n_actions):
-    def policyFunction(state, epsilon):
+def createEpsilonGreedyPolicy(n_actions):
+    def policyFunction(Q, state, epsilon):
         action_probabilities = np.ones(n_actions,
                                        dtype=float) * epsilon / n_actions
 
@@ -55,13 +55,20 @@ class DVFSAgent:
         power_consumption_reward = s_prime[_POWER_KEY] / s[_POWER_KEY] - 1
         return self.alpha1 * tr_reward + self.alpha2 * ar_reward + self.alpha3 * power_consumption_reward
 
-    def get_s_prime(self, s, T, w, ):
+    #  assumption: current p is added to power_hist before head
+    def anticipate_s_prime(self, T, w, action):
         t = time_ns() - self.start_time
-        p, r, tr = s
         next_p = self.power_predictor.predict(np.array(self.power_hist))
         next_tr = self.tr.get_TR(t, T)
-        next_r = self.fr.get_FR()
+        V, f = action
+        next_r = self.fr.get_FR(w, f, V)
+        return self.get_state((next_p, next_r, next_tr))
 
+    def update_Q(self, Q, reward, s, action, s_prime):
+        best_next_action = np.argmax(Q[s_prime])
+        td_target = reward + self.discount_factor * Q[s_prime][best_next_action]
+        td_delta = td_target - Q[s][action]
+        Q[s][action] += self.lr * td_delta
 
     def get_state(self, s):
         v, r, tr = s
@@ -71,6 +78,17 @@ class DVFSAgent:
         v = array[idx]
         return v, r, tr
 
+    @staticmethod
+    def get_initial_state():
+        # TODO
+        return 0, 0, 0
+
+    def env_step(self, s, action):
+        # TODO
+        T = 10
+        w = 1
+        return (0, 0, 0), T, w
+
     def start(self, n_episodes, epsilon=0.1):
         n_actions = self.action_space.shape[0]
         Q = defaultdict(lambda: np.zeros(n_actions))
@@ -79,33 +97,36 @@ class DVFSAgent:
             episode_lengths=np.zeros(n_episodes),
             episode_rewards=np.zeros(n_episodes))
 
-        policy = createEpsilonGreedyPolicy(Q, n_actions)
-
+        policy = createEpsilonGreedyPolicy(n_actions)
+        action = None
         for ith_episode in range(n_episodes):
 
-            # state = env.reset()
+            state, w, T = self.get_initial_state()
 
             for t in itertools.count():
-                pass
-                # action_probabilities = policy(state)
 
-                # action = np.random.choice(np.arange(
-                #     len(action_probabilities)),
-                #     p=action_probabilities)
+                cur_epsilon = np.exp(np.log(epsilon / (t + 1)))
+                Q_prime = Q.copy()
+                if action:
+                    anticipated_next_state = self.anticipate_s_prime(T, w, action)
+                    anticipated_reward = self.get_reward(state, anticipated_next_state)
+                    self.update_Q(Q_prime, anticipated_reward, state, action, anticipated_next_state)
 
-                # next_state, reward, done, _ = env.step(action)
-                #
-                # stats.episode_rewards[ith_episode] += reward
-                # stats.episode_lengths[ith_episode] = t
-                #
-                # best_next_action = np.argmax(Q[next_state])
-                # td_target = reward + discount_factor * Q[next_state][best_next_action]
-                # td_delta = td_target - Q[state][action]
-                # Q[state][action] += alpha * td_delta
+                action_probabilities = policy(Q_prime, state, cur_epsilon)
 
-                # if done:
-                #     break
+                action = np.random.choice(np.arange(
+                    len(action_probabilities)),
+                    p=action_probabilities)
 
-                # state = next_state
+                next_state = self.env_step(state, action)
+
+                self.power_hist.append(state[0])
+                reward = self.get_reward(state, next_state)
+
+                stats.episode_rewards[ith_episode] += reward
+                stats.episode_lengths[ith_episode] = t
+
+                self.update_Q(Q, reward, state, action, next_state)
+                state = next_state
 
         return Q, stats
